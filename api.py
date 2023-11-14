4d4c93121d60f11824cdb05aa5626ae81c506649
luluw <shashinmhrzn@gmail.com>
2023-11-14 21:23:47 +0545

    Algolia Search and TMDB Integration

--- api.py
+++ api.py
@@ -1,20 +1,37 @@
 import requests
 
 # TMDB API key
-TMDB_API_KEY = "#"
+TMDB_API_KEY = '#'
 
+# Algolia Search API key
+ALGOLIA_APP_ID = '#'
+ALGOLIA_API_KEY = '#'
+ALGOLIA_INDEX_NAME = '#'
+
+# TMDB API fetch Request 
 def fetch_poster(movie_ids):
     poster_urls = []
     for movie_id in movie_ids:
-        url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={TMDB_API_KEY}&language=en-US"
+        url = f"https://api.themoviedb.org/3/movie/{movie_id}&language=en-US?api_key={TMDB_API_KEY}"
         data = requests.get(url)
         data = data.json()
-        poster_path = data['poster_path']
-        full_path = f"https://image.tmdb.org/t/p/w500/{poster_path}"
-        poster_urls.append(full_path)
+        # Check 'poster_path' exists in response
+        if 'poster_path' in data:
+            poster_path = data['poster_path']
+            full_path = f"https://image.tmdb.org/t/p/w500/{poster_path}"
+            poster_urls.append(full_path)
+        else:
+            poster_urls.append(None)
+
     return poster_urls
 
-# Check Api fetch request
+
+# TEST1: TMDB Api fetch request
 # movie_ids = [19995] 
-# posters = fetch_poster(movie_ids, api_key=TMDB_API_KEY)
+# posters = fetch_poster(movie_ids)
 # print(posters)
+
+# TEST2: ALgolia Search API fetch request
+
+# Check Trailer fetch request
+# https://api.themoviedb.org/3/movie/19995/videos?api_key=58d7bb45cdb3261fb790a798152d9e93&language=en-US
--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, url_for
-from api import fetch_poster
 import pickle
 from math import ceil
 import re
@@ -7,18 +6,27 @@ import re
 import pandas as pd
 import numpy as np
 
-# Load dataset files
+from algoliasearch.search_client import SearchClient
+from api import fetch_poster, ALGOLIA_APP_ID, ALGOLIA_API_KEY, ALGOLIA_INDEX_NAME
+
+# Algolia Search
+client = SearchClient.create(ALGOLIA_APP_ID, ALGOLIA_API_KEY)
+index = client.init_index(ALGOLIA_INDEX_NAME)
+
+
+# ------Load dataset files--------
 popular_df = pickle.load(open('model/movies_dataset.pkl', 'rb'))
 
 app = Flask(__name__)
 
-# Static location config
+# ------Static location config-------
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config['UPLOAD_FOLDER'] = 'static'
 app.secret_key = 'secret'
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['UPLOAD_FOLDER'] = 'static'
 
+# --------Templates----------
 # Setting up home page
 @app.route('/')
 def index():
@@ -47,7 +55,7 @@ def index():
     start_index = (page - 1) * per_page
     end_index = start_index + per_page
 
-    paginated_movies = movies_data.iloc[start_index:end_index] 
+    paginated_movies = movies_data.iloc[start_index:end_index]
 
     title = list(paginated_movies['title'].values)
     crew = list(paginated_movies['crew'].values)
@@ -60,11 +68,53 @@ def index():
     next = '/?page=' + str(page + 1) if page < num_pages else None
 
     return render_template('home.html', movies=paginated_movies.to_dict(orient='records'),
-                           title=title, 
-                           crew=crew, popularity=popularity, 
-                           posters=posters, 
+                           title=title,
+                           crew=crew, popularity=popularity,
+                           posters=posters,
                            num_pages=num_pages, current_page=page, prev=prev, next=next)
+
+# Search function
+@app.route('/search')
+def search():
+    query = request.args.get('query')
+    format = request.args.get('format', 'html')
+
+    if not query:
+        if format == 'json':
+            return jsonify({'error': 'query parameter is required'})
+        else:
+            return render_template('search.html',
+                                   error='query parameter is required')
+
+    results = index.search(query)
+    hits = results['hits']
+
+    # Extract movie IDs from search results
+    movie_ids = [hit['id'] for hit in hits]
+
+    # Fetch poster URLs for the movie IDs
+    posters = fetch_poster(movie_ids)
+
+    # Add poster URLs to the search results
+    for hit, poster_url in zip(hits, posters):
+        hit['poster_url'] = poster_url
+
+    if format == 'json':
+        return jsonify(hits)
+    else:
+        return render_template('search.html', hits=hits, query=query,
+                               posters=posters)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+# Test 
+# movies = pickle.load(open('model/movie_list.pkl','rb'))
+# similarity = pickle.load(open('model/similarity.pkl','rb'))
+
+# movie_list = movies['title'].values
+# selected_movie = st.selectbox(
+#     "Type or select a movie from the dropdown",
+#     movie_list
+# )
