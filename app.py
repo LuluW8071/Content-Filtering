44d5d254e5fbab6302576887dde469e1e7a8b1ec
luluw <shashinmhrzn@gmail.com>
2023-11-20 19:17:54 +0545

    Final Changes

--- app.py
+++ app.py
@@ -152,16 +152,3 @@ def movie():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-# Test
-# movies = pickle.load(open('model/movie_list.pkl','rb'))
-# similarity = pickle.load(open('model/similarity.pkl','rb'))
-
-# movie_list = movies['title'].values
-# selected_movie = st.selectbox(
-#     "Type or select a movie from the dropdown",
-#     movie_list
-# )
-
- # Testing
- # Recommendation through movies_df and similarity
